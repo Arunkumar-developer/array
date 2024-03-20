aec07a9385b292b9a49f8f1764f4acd17ea33a62
Arunkumar-developer <142041785+Arunkumar-developer@users.noreply.github.com>
2024-03-20 07:37:42 +0530

    array

---
+++ index.js
@@ -0,0 +1,74 @@
+// An array in JavaScript is a data structure that stores a collection of elements in a specific order.
+// 1.push ;-
+// Adds one or more elements to the end of an array.
+// var arr=["apple","banana","orange","grapes","watermilon","kiwi"];
+// var b=arr.push("peach")
+// console.log(arr);
+
+// 2.pop ;-
+// remove the last element fron an Array
+// var arr=["apple","banana","orange","grapes","watermilon","kiwi"];
+// var b=arr.pop()
+// console.log(arr);
+// console.log(b);
+
+// 3.shift ;-
+// removes the frist element from the Array
+// var arr=["apple","banana","orange","grapes","watermilon","kiwi"];
+// var b=arr.shift()
+// console.log(arr);
+
+// 4.unshift ;-
+// add one are more elements in the begining of the Array
+// var arr=["apple","banana","orange","grapes","watermilon","kiwi"];
+// var b=arr.unshift("mango")
+// console.log(arr);
+
+// 5.concat
+// the concat() method creates a new array by merging (cooncatinating) existing arrays;
+// var arr=["apple","banana","orange",];
+// var b=arr.concat("grapes","watermilon","kiwi");
+// console.log(b);
+
+// 6.flat
+// Create a new array with the sub-array elements concatenated:
+// var arr=["apple",["banana",["orange","grapes"],"watermilon"],"kiwi"];
+// var arr2=arr.flat();
+// console.log(arr);
+
+// 7.slice
+// The slice() method returns selected elements in an array, as a new array.
+// The slice() method selects from a given start, up to a (not inclusive) given end.
+// The slice() method does not change the original array.
+// var arr=["apple","banana","orange","grapes","watermilon","kiwi"];
+// var arr1=arr.slice(2,6);
+// console.log(arr1);
+
+
+// 8.splice
+// The splice() method adds and/or removes array elements.
+// The splice() method overwrites the original array.
+// var arr=["apple","banana","orange","grapes","watermilon","kiwi"];
+// var aa1=arr.splice(2,4,5);
+// console.log(arr);
+
+
+// 9.Join
+// returns an array as a string doesn't change the original array
+// var arr=["apple","banana","orange",];
+// var b=arr.join(" and ");
+// console.log(b);
+
+
+
+
+
+
+
+// var n;
+// for (n = theArray.length - 2; n >= removeAt; --n) {
+//     theArray[n] = theArray[n+1];
+// }
+// --theArray.length;
+
+
